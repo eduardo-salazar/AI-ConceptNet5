2a1b0304af36a8cfb6077723f1ef088295cf7725
eduardo-salazar <esalazar922@gmail.com>
2015-12-13 21:33:50 +0800

    Testing callback using test examples

--- src/com/ai/Main.java
+++ src/com/ai/Main.java
@@ -1,8 +1,18 @@
 package com.ai;
+import com.ai.contentnet5.services.Search;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code her
+        System.out.println("Testing first concept net call");
+        try {
+            System.out.println(new Search("teacher","").call().toString());
+        } catch (IOException e) {
+            System.out.println("Error getting response:");
+            e.printStackTrace();
+        }
     }
 }
