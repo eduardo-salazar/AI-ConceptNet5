12c96991e109e9190c2045e9d940d51dfbcdb9e7
eduardo-salazar <esalazar922@gmail.com>
2015-12-13 22:20:37 +0800

    Created methods to test search

---
+++ test/com/ai/contentnet5/services/SearchTest.java
@@ -0,0 +1,26 @@
+package com.ai.contentnet5.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by eduardosalazar1 on 12/13/15.
+ */
+public class SearchTest {
+
+    @Test
+    public void testSetStart() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testCall() throws Exception {
+
+    }
+}
