2b69d087c3665f4539a04f5033486d9003d1c273
eduardo-salazar <esalazar922@gmail.com>
2016-01-03 02:46:49 +0800

    Added filters to remove Relations

--- src/com/ai/Main.java
+++ src/com/ai/Main.java
@@ -1,6 +1,7 @@
 package com.ai;
+import com.ai.conceptnet5.Relation;
 import com.ai.conceptnet5.services.Search;
-
+import java.util.List;
 import java.io.IOException;
 
 public class Main {
@@ -9,7 +10,11 @@ public class Main {
 	// write your code her
         System.out.println("Testing first concept net call");
         try {
-            System.out.println(new Search("teacher","").call());
+
+            List<Relation> list = new Search("teacher","").call();
+            for(int i=0;i<list.size();i++){
+                System.out.println(list.get(i));
+            }
         } catch (IOException e) {
             System.out.println("Error getting response:");
             e.printStackTrace();
--- src/com/ai/conceptnet5/services/Search.java
+++ src/com/ai/conceptnet5/services/Search.java
@@ -45,7 +45,37 @@ public class Search {
         String uri = SEARCHENDPOINT+start+'&'+end;
         System.out.println("Trying to request "+ uri);
         String response = Request.Get(uri).execute().returnContent().toString();
-        return Helper.parseResult(response);
+        List<Relation> elements = Helper.parseResult(response);
+
+        //Firs filter is to remove relationships with extrems null
+        removeNulls(elements);
+
+        //Second remove those relationships lower than 1
+        removeWeightLowerThan(elements,new Double(1));
+
+        return elements;
+    }
+
+    public void removeNulls(List<Relation> input){
+        for(int i=0;i<input.size();i++){
+            String startText = input.get(i).getSurfaceStart();
+            String endText = input.get(i).getSurfaceEnd();
+            if(startText.equals("") || endText.equals("")){
+                input.remove(i);
+                --i;
+            }
+        }
+
+    }
+
+    public void removeWeightLowerThan(List<Relation> input,Double value){
+        for(int i=0;i<input.size();i++){
+            if(input.get(i).getWeight()< value){
+                input.remove(i);
+                --i;
+            }
+        }
+
     }
 
 
